9323f7f55762d83132cdb69fb973a72ae97aae36
Umamah Faisal Mirza <21UMirza@heckgrammar.co.uk>
2024-06-26 13:42:13 +0100

    3

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Services;
 using System.Text;
@@ -21,8 +22,8 @@ namespace Y9_DEC_TO_BIN_SKELETON
 
             Console.WriteLine("Enter a number");
             int h = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i <= h; i++) {
-                h = h - 1;
+            for (int i = 0; i < h; i++) {
+                h = h -1;
                 Console.WriteLine(h);
                 
             }
@@ -41,7 +42,7 @@ namespace Y9_DEC_TO_BIN_SKELETON
 
 
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
+           /* int myInt = 0; *///just for testing single stepping
             string myString = "12"; //watch me being cast from string to int
             int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
@@ -50,7 +51,8 @@ namespace Y9_DEC_TO_BIN_SKELETON
             //CODE GOES HERE
             Console.WriteLine("Enter denary number");
             int Usernum = Convert.ToInt32(Console.ReadLine());
-            numberConversion(Usernum, 2);
+            Console.WriteLine(numberConversion(Usernum, 2)); 
+            
         }
 
         //static void means the function will not return a value so it does not need a data type 
@@ -59,21 +61,30 @@ namespace Y9_DEC_TO_BIN_SKELETON
         {
             int y = 0;
             int i = 0;
+            int bitNum = 0;
+            string[] Binary = { };
+            string result = "";
+
             while ( i == 0)
             {
                 int x = number % numberbase;
                 string Remainder = Convert.ToString(x);
-                string[] Binary = { };
+                
                 Binary[y]= Remainder;
                 y = y++;
                  number = number / numberbase;
-
+                bitNum = bitNum++;
                 if (0 == number) 
                 {
                     i = i++;
                 }
             };
-
+            
+            for (int h = 0; h == bitNum ; h++)
+            {
+                
+                    result = result + Binary[h] ; 
+            }
 
             return result; //REMOVE THE RED LINE!
         }
