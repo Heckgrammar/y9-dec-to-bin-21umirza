e79781c873e09ffc105dde4e70ac0152fe2752d8
Umamah Faisal Mirza <21UMirza@heckgrammar.co.uk>
2024-07-02 15:14:24 +0100

    3

--- Program.cs
+++ Program.cs
@@ -18,42 +18,44 @@ namespace Y9_DEC_TO_BIN_SKELETON
 
 
 
-            ///TASK 1: CONVERT AN INTEGER TO A STRING
+            /////TASK 1: CONVERT AN INTEGER TO A STRING
 
-            Console.WriteLine("Enter a number");
-            int h = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < h; i++)
-            {
-                h = h - 1;
-                Console.WriteLine(h);
+            //Console.WriteLine("Enter a number");
+            //int h = Convert.ToInt32(Console.ReadLine());
+            //int b = h;
+            //for (int i = 0; i < b; i++)
+            //{
+            //    h = h - 1;
+            //    Console.WriteLine(h);
 
-            }
-
-
-
-
-            ///TASK 2: INSERT A STRING AT THE START OF ANOTHER STRING
-
-            Console.WriteLine("enter something");
-            string first = Console.ReadLine();
-            Console.WriteLine("enter something");
-            string second = Console.ReadLine();
-            string final = first + second;
-            Console.WriteLine(final);
+            //}
 
 
-            //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            /* int myInt = 0; *///just for testing single stepping
-            string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
+
+
+            /////TASK 2: INSERT A STRING AT THE START OF ANOTHER STRING
+
+            //Console.WriteLine("enter something");
+            //string first = Console.ReadLine();
+            //Console.WriteLine("enter something");
+            //string second = Console.ReadLine();
+            //string final = first + second;
+            //Console.WriteLine(final);
+
+
+            ////STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
+            ///* int myInt = 0; *///just for testing single stepping
+            //string myString = "12"; //watch me being cast from string to int
+            //int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
             //MAIN:  NUMBER CONVERSION PROGRAM
 
             //CODE GOES HERE
             Console.WriteLine("Enter denary number");
             int Usernum = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine(numberConversion(Usernum, 2)); 
+            Console.WriteLine("pLEASE ENTER THE BASE");
+            int numberbase = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numberConversion(Usernum, numberbase));
             
         }
 
@@ -63,33 +65,35 @@ namespace Y9_DEC_TO_BIN_SKELETON
         {
             int y = 0;
             int i = 0;
-            int bitNum = 0;
-            string userNum = Convert.ToString(number);
-            int length = userNum.Length;
-           new string[length] Binary = { };
+           
+            //string userNum = Convert.ToString(number);
+            //int length = userNum.Length;
+            int[] Binary = new int[8]; 
+            
             string result = "";
 
             while ( i == 0)
             {
-                int x = number % numberbase;
-                string Remainder = Convert.ToString(x);
+                 Binary[y] = number % numberbase;
                 
-                Binary[y]= Remainder;
+                
+                
                 y = y++;
-                 number = number / numberbase;
-                bitNum = bitNum++;
-                if (0 == number) 
-                {
-                    i = i++;
-                }
-            };
-            
-            for (int h = 0; h <= 8 ; )
-            {
                 
-                    result = result + Binary[h] ; 
-                h = h + 1;
+                number = number / numberbase;
+                
+                
+            };
+            int m = Binary.Length;
+            int l = 0;
+            while (m != 0)
+            {
+                result = Binary [m - 1];
+                m--;
+                l++;
             }
+            if (number == 0) {
+            Console.WriteLine(result);
 
             return result; //REMOVE THE RED LINE!
         }
